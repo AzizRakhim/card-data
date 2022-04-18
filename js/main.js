d9aee7720d05b2a62619e4825b27c604a3a90a26
AzizRakhim <azizbekrahimjonov80@gmail.com>
2022-04-18 17:45:31 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,89 @@
+let elList = document.querySelector(".hero__list");
+
+let elRequest = new XMLHttpRequest();
+
+elRequest.open("GET", "https://reqres.in/api/users");
+
+elRequest.onload = function() {
+  let elData = JSON.parse(elRequest.responseText);
+
+  showHTML(elData);
+}
+
+elRequest.send();
+
+elCount = 1;
+function showHTML(elData){
+  elData.data.forEach((element) => {
+    let elItem = document.createElement("li");
+    elItem.className = "hero__item col-12 col-md-6 col-lg-4 position-relative animate__animated animate__fadeInDown";
+
+    elItem.innerHTML = `
+      <div class="hero__border-item">
+        <div class="hero__img-info d-flex align-items-center">
+          <div class="hero__img-box">
+            <img src="${element.avatar}" alt="user icon">
+          </div>
+          <div class="hero__name-box">
+            <h2 class="hero__name">
+              ${element.first_name}
+            </h2>
+            <p class="hero__full-name">
+              ${element.first_name + ' ' + element.last_name}
+            </p>
+          </div>
+        </div>
+        <ul class="hero__inner-list">
+          <li class="hero__inner-item d-flex align-items-center justify-content-between">
+            <p class="hero__inner-title">
+              company
+            </p>
+            <p class="hero__inner-text">
+              Romaguera-Crona
+            </p>
+          </li>
+          <li class="hero__inner-item d-flex align-items-center justify-content-between">
+            <p class="hero__inner-title">
+              email
+            </p>
+            <p class="hero__inner-text">
+              ${element.email}
+            </p>
+          </li>
+          <li class="hero__inner-item d-flex align-items-center justify-content-between">
+            <p class="hero__inner-title">
+              phone
+            </p>
+            <p class="hero__inner-text">
+              1-770-736-8031 x56442
+            </p>
+          </li>
+          <li class="hero__inner-item d-flex align-items-center justify-content-between">
+            <p class="hero__inner-title">
+              website
+            </p>
+            <p class="hero__inner-text">
+              hildegard.org
+            </p>
+          </li>
+        </ul>
+      </div>
+      <button class="hero__btn btn btn-danger">
+        remove user
+      </button>
+    `;
+
+    elList.appendChild(elItem);
+  });
+
+  let elBtns = document.querySelectorAll(".hero__btn");
+  deleteIt(elBtns);
+}
+
+function deleteIt(elBtns){
+  elBtns.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      e.target.parentNode.remove();
+    });
+  }); 
+}
